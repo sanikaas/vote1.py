2bf97770ab366a365b8748e904055e70824cc8af
sanikaas <sanikaas13@gmail.com>
2025-10-06 11:25:42 +0530

    save

---
+++ vote1.py
@@ -0,0 +1,10 @@
+# voting_eligibility.py
+
+# Get user's age
+age = int(input("Enter your age: "))
+
+# Check eligibility
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
